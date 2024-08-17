3802bb4e911a11471b721aee76ba9da301f16afc
BlinkStarDesigns <blessedlionel20@gmail.com>
2024-08-17 11:41:07 +0200

    changed logo and made changes to the footer

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -40,12 +40,12 @@ const Profile = () => {
   };
 
   return (
-    <div>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
       <Space direction="vertical" size={16}>
         <Card
           title="My Profile"
           extra={<a onClick={showModal}>EDIT</a>}
-          style={{ width: 300 }}
+          style={{ width: 500 }}
         >
           <p>{user.name}</p>
           <p>{user.email}</p>
